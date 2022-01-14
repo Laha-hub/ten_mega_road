3f2bea458324858e98a0d4c8065f16ccd99fd96d
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2022-01-14 18:41:19 +0900

    #4 Create and Customize Testimonials Section with Video Background

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 {
-    // Array.from(document.querySelectorAll('.navigation-button'))
+    // Array.from(document.querySelectorAll('.navigation-button')).forEach(item => {
     document.querySelectorAll('.navigation-button').forEach(item => {
         item.addEventListener('click', () => {
             item.parentElement.parentElement.classList.toggle('change'); // item.p~.p~ = .card
