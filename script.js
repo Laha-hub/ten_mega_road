848378ee8345dfc8143ef8d1cd624e552d431525
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2022-01-16 22:56:06 +0900

    #7 Create and Make Navigation Work

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 'use strict';
 
 {
-    // Array.from(document.querySelectorAll('.navigation-button')).forEach(item => {
+    // navbar
+    const container = document.querySelector('.container');
+    document.querySelector('.open-navbar-icon').addEventListener('click', () => {
+        container.classList.add('change');
+    });
+
+    document.querySelector('.close-navbar-icon').addEventListener('click', () => {
+        container.classList.remove('change');
+    });
+
+
+    const colors = [
+        "#6495ed",
+        "#7fffd4",
+        "#ffa07a",
+        "#f08080",
+        "#afeeee",
+    ];
+
+    let i = 0;
+
+    document.querySelectorAll('.nav-link').forEach(item => {
+        item.style.backgroundColor = colors[i++];
+    });
+
+
+    // popular-tours
     document.querySelectorAll('.navigation-button').forEach(item => {
         item.addEventListener('click', () => {
             item.parentElement.parentElement.classList.toggle('change'); // item.p~.p~ = .card
